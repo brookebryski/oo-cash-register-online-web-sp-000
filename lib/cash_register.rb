683c78f57eabdae06d8180be7eaadb73f1690f4e
chic-state-0895 <brookeabryski@gmail.com>
2020-09-15 17:14:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :total, :discount,:titles, :items,:price
+  attr_accessor :total, :discount,:titles, :items,:price, :last_transaction
 
 def initialize(discount = 0)
   @total = 0
